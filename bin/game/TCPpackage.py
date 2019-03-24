dd73ec04bd515f5cfe8337317c8eec38de09ac5e
BaiAsuka <296010236@qq.com>
2019-03-24 16:09:43 +0800

    增加TCP包类用于防粘包

---
+++ bin/game/TCPpackage.py
@@ -0,0 +1,27 @@
+import pickle
+import copy
+
+# 通过在数据前增加数据长度段来分割粘连包
+# 数据长度段的长度为4字节
+
+class TCPpackage:
+    def __init__(self, pck=None):
+        if pck:
+            self._content = bytearray(pck)
+        else:
+            self._content = bytearray(0)
+
+    def get_content(self):
+        try:
+            content = self._content[4:]
+            return content.decode(encoding='utf-8')
+        except:
+            raise Exception("解析数据异常！")
+
+    def set_content(self,msg):
+        self._content = bytearray(msg.encode(encoding='utf-8'))
+
+    def get_pck_with_head(self):
+        pck_length = bytearray(len(self._content).to_bytes(4, byteorder='little'))
+        return pck_length + self._content
+
